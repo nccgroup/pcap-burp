0d3d5dcf9817e7e6b154647e0a267a7b123d87c4
Stephen Tomkinson <stephen.tomkinson@nccgroup.com>
2015-02-26 10:32:03 +0000

    Added license information for public release

--- src/com/nccgroup/burp/HttpUtils.java
+++ src/com/nccgroup/burp/HttpUtils.java
@@ -1,3 +1,13 @@
+/* 
+ * Released as open source by NCC Group Plc - https://www.nccgroup.com/
+ *
+ * Developed by Stephen Tomkinson, stephen.tomkinson@nccgroup.com
+ * 
+ * https://www.github.com/nccgroup/pcap-burp
+ *
+ * Licensed under the Affero General Public License 
+ * https://github.com/nccgroup/pcap-burp/LICENSE
+ */
 package com.nccgroup.burp;
 
 import java.io.ByteArrayInputStream;
